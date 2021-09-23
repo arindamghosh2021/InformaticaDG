90b899b1e3f82ab9a47d380bff2785e596709bd5
arindamghosh2021 <91253100+arindamghosh2021@users.noreply.github.com>
2021-09-23 14:48:24 +0530

    Add files via upload

---
+++ Subscribe_Dataset_Datadomain.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 27 00:07:35 2021
+
+@author: sjain
+"""
+
+import pandas as pd
+import requests
+import json
+#import os as os
+#import numpy as np
+import logging  
+
+logger=logging.getLogger()
+logger.setLevel(logging.DEBUG)
+#import requests
+requests.packages.urllib3.disable_warnings(
+    requests.packages.urllib3.exceptions.InsecureRequestWarning)
+
+search_url='https://inedclab0101.informatica.com:9085/access/2/catalog/data/search'
+username='Administrator'
+password='Administrator'
+#param_q={'core.classType':'*.Table','core.resourceName':'sales_sachin'}
+payload={
+          'q':'com.infa.ldm.profiling.dataDomainsAccepted:Customer_ID'
+         # 'attributeId': 'com.infa.ldm.profiling.lastProfileTime'
+        }
+        
+headers = {
+  'Content-Type': 'application/json',
+  'Accept': 'application/json',
+  'Authorization': 'Basic QWRtaW5pc3RyYXRvcjpBZG1pbmlzdHJhdG9y'
+ #'Authorization': 'Basic QWRtaW5pc3RyYXRvcjpBZG1pbmlzdHJhdG9y'
+}
+from requests.auth import HTTPBasicAuth
+get_response = requests.get(search_url,auth=HTTPBasicAuth(username, password),params = payload, headers=headers,verify=False)
+
+pr_getvalueJson = get_response.json()
+get_list=pr_getvalueJson ['hits']
+
+df=pd.DataFrame(get_list)
+df=df.drop(['href','nativeId','values','subHits'], axis = 1)
+
+df['Table_name'] = df.id.str.extract(r'\b(\w+)$',expand = True)
+
+df['bar'] = df.apply(lambda row : row['id'].replace(str(row['Table_name']), ''), axis=1)
+df['bar'] = df.bar.str[:-1]
+
+def subscribe(row):
+    print("name of the table")
+    #print (row['bar'])
+    URL_subscr= 'https://inedclab0101.informatica.com:9085/access/2/catalog/data/subscriptions'
+    
+    payload_subs = {
+            #{
+         "items": [
+         {
+      "changeTypes": [
+        "SOURCE",
+        "INFERENCES_ANNOTATIONS",
+        "COLLABORATION"
+      ],
+      #"objectId": "sales_sachin://PDB01/SJAINA/ORC_TGT"
+      "objectId":row['bar']
+      }
+    ]
+   }
+       
+    #print (json.dumps(payload_subs))
+    requests.put(URL_subscr, auth=HTTPBasicAuth(username, password),headers=headers, data=json.dumps(payload_subs),verify=False)
+    #print (a)
+ 
+countFlag=0
+#print (df.bar.count())
+for index, row in df.iterrows():
+    if (df.bar.count())>0:
+        result=subscribe(row)
+        #print(result)
+    else:
+        pass
+#    
